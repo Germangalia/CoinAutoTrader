69a31e77fbd833e5eff1707be424e6f967f59af0
Germangalia <germangalia@iesebre.com>
2016-04-19 20:37:16 +0200

    Implement methods in CoinBaseDeposits class.

--- app/CoinBaseAPI/CoinBaseDeposits.php
+++ app/CoinBaseAPI/CoinBaseDeposits.php
@@ -1,12 +1,42 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: ggalia84
- * Date: 18/04/16
- * Time: 20:53
- */
+use Coinbase\Wallet\Enum\CurrencyCode;
+use Coinbase\Wallet\Resource\Deposit;
+use Coinbase\Wallet\Value\Money;
+use Coinbase\Wallet\Enum\Param;
+
 class CoinBaseDeposits
 {
+
+    //List deposits
+    public function getDeposits($client, $account)
+    {
+        $deposits = $client->getAccountDeposits($account);
+        return $deposits;
+    }
+
+
+    //Get deposit info
+    public function getDepositInfo($client, $account, $depositId)
+    {
+        $deposit = $client->getDeposit($account, $depositId);
+        return $deposit;
+    }
+
+
+    //Deposit funds
+    public function setDepositFounds($client, $account, $amount)
+    {
+        $deposit = new Deposit();
+        $deposit->setAmount($amount, CurrencyCode::BTC);
+
+        $client->createAccountDeposit($account, $deposit);
+
+        //Commit a deposit
+        //You only need to do this if you pass commit=false when you create the deposit.
+        //$client->createAccountDeposit($account, $deposit, [Param::COMMIT => false]);
+        //$client->commitDeposit($deposit);
+    }
+
 
 }
