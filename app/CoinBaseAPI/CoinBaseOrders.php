e4ecbe13d2efe05fada58926778caa4b9a7caec7
Germangalia <germangalia@iesebre.com>
2016-04-19 20:53:25 +0200

    Implement methods in CoinBaseOrders class.

--- app/CoinBaseAPI/CoinBaseOrders.php
+++ app/CoinBaseAPI/CoinBaseOrders.php
@@ -1,12 +1,43 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: ggalia84
- * Date: 18/04/16
- * Time: 20:53
- */
+use Coinbase\Wallet\Resource\Order;
+use Coinbase\Wallet\Value\Money;
+use Coinbase\Wallet\Enum\CurrencyCode;
+
 class CoinBaseOrders
 {
+
+    //List orders
+    public function getOrders($client)
+    {
+        $orders = $client->getOrders();
+        return $orders;
+    }
+
+
+    //Get order
+    public function getOrderById($client, $orderId)
+    {
+        $order = $client->getOrder($orderId);
+        return $order;
+    }
+
+
+    //Create order
+    public function createOrder($client, $amount)
+    {
+        $order = new Order();
+        $order->setName('Order #1234');
+        $order->setAmount($amount);
+
+        $client->createOrder($order);
+    }
+
+
+    //Refund order
+    public function refundOrder($client, $order)
+    {
+        $client->refundOrder($order, CurrencyCode::BTC);
+    }
 
 }
