bc69c356ca3b0114c07451d99b61b0dbcd456182
Germangalia <germangalia@iesebre.com>
2016-04-19 20:24:06 +0200

    Implement methods in CoinBaseBuys class.

--- app/CoinBaseAPI/CoinBaseBuys.php
+++ app/CoinBaseAPI/CoinBaseBuys.php
@@ -1,12 +1,35 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: ggalia84
- * Date: 18/04/16
- * Time: 20:52
- */
+use Coinbase\Wallet\Resource\Buy;
+use Coinbase\Wallet\Enum\Param;
+
 class CoinBaseBuys
 {
+
+    //List buys
+    public function getBuys($client, $account)
+    {
+        $buys = $client->getAccountBuys($account);
+        return $buys;
+    }
+
+
+    //Get buy info
+    public function getBuyInfo($client, $account, $buyId)
+    {
+        $buy = $client->getAccountBuy($account, $buyId);
+        return $buy;
+    }
+
+
+    //Buy bitcoins
+    public function buyBitcoins($client, $account, $amount)
+    {
+        $buy = new Buy();
+        $buy->setBitcoinAmount($amount);
+
+        $client->createAccountBuy($account, $buy, [Param::COMMIT => false]);
+        $client->commitBuy($buy);
+    }
 
 }
