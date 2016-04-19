661fa6be6a60d0e69d9cb9b50812d480cdb577ca
Germangalia <germangalia@iesebre.com>
2016-04-19 19:55:31 +0200

    Implement methods in CoinBaseTransactions class.

--- app/CoinBaseAPI/CoinBaseTransactions.php
+++ app/CoinBaseAPI/CoinBaseTransactions.php
@@ -3,49 +3,97 @@
 use Coinbase\Wallet\Enum\CurrencyCode;
 use Coinbase\Wallet\Resource\Transaction;
 use Coinbase\Wallet\Value\Money;
+use Coinbase\Wallet\Resource\Account;
 
 class CoinBaseTransactions
 {
 
-    //Client authenticated
-    protected $client;
-
-    //Account
-    protected $account;
-
-    //Primary account
-    protected $primaryAcount;
-
-    //Address
-    protected $address;
-
-    /**
-     * CoinBaseTransactions constructor.
-     * @param $client
-     * @param $account
-     * @param $primaryAcount
-     * @param $address
-     */
-    public function __construct($client, $account, $primaryAcount, $address)
+    // Send coins from the account to primary account:
+    public function setTransaction($client, $address, $primaryAccount, $amount)
     {
-        $this->client = $client;
-        $this->account = $account;
-        $this->primaryAcount = $primaryAcount;
-        $this->address = $address;
-    }
-
-
-    public function setTransaction($amount)
-    {
-        // Send coins from the account to primary account:
-
         $transaction = Transaction::send();
-        $transaction->setToBitcoinAddress($this->address->getAddress());
+        $transaction->setToBitcoinAddress($address->getAddress());
         $transaction->setAmount(new Money($amount, CurrencyCode::BTC));
         $transaction->setDescription('For being awesome!');
+        if($amount < 0.0001)
+        {
+            $transaction->setFee(0.0001);
+        }
 
-        $this->client->createAccountTransaction($this->primaryAccount, $transaction);
+        $client->createAccountTransaction($primaryAccount, $transaction);
         print_r($transaction);
     }
+
+
+    //List transactions
+    public function getTransactions($client, $account)
+    {
+        $transactions = $client->getAccountTransactions($account);
+        return $transactions;
+    }
+
+
+    //Get transaction info
+    public function getTransactionInfo($client, $account, $transactionId)
+    {
+        $transaction = $client->getAccountTransaction($account, $transactionId);
+        return $transaction;
+    }
+
+
+    //Transfer funds to a new account
+    public function setTransferFoundsToNewAccount($client, $accountId, $toAccount, $amount)
+    {
+        $fromAccount = Account::reference($accountId);
+
+        $toAccount = new Account([
+            'name' => 'New Account'
+        ]);
+        $client->createAccount($toAccount);
+
+        $transaction = Transaction::transfer([
+            'to'            => $toAccount,
+            'bitcoinAmount' => $amount,
+            'description'   => 'Your first bitcoin!'
+        ]);
+
+        $client->createAccountTransaction($fromAccount, $transaction);
+    }
+
+
+    //Request funds
+    public function getRequestFounds($client, $amount)
+    {
+        $transaction = Transaction::request([
+            'amount'      => new Money($amount, CurrencyCode::BTC),
+            'description' => 'Burrito'
+        ]);
+
+        $client->createAccountTransaction($transaction);
+    }
+
+
+
+
+    //Resend request
+    public function resendRequest($account, $transaction)
+    {
+        $account->resendTransaction($transaction);
+    }
+
+
+    //Cancel request
+    public function cancelRequest($account, $transaction)
+    {
+        $account->cancelTransaction($transaction);
+    }
+
+
+    //Fulfill request
+    public function fulfillRequest($account,$transaction)
+    {
+        $account->completeTransaction($transaction);
+    }
+
 
 }
