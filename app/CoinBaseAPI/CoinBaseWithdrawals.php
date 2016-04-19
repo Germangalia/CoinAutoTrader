5821dd2acf77ddf43883af1516dfcbd529ae8f5d
Germangalia <germangalia@iesebre.com>
2016-04-19 20:43:20 +0200

    Implement methods in CoinBaseWithdraws class.

--- app/CoinBaseAPI/CoinBaseWithdrawals.php
+++ app/CoinBaseAPI/CoinBaseWithdrawals.php
@@ -1,12 +1,40 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: ggalia84
- * Date: 18/04/16
- * Time: 20:55
- */
+use Coinbase\Wallet\Enum\CurrencyCode;
+use Coinbase\Wallet\Resource\Withdrawal;
+use Coinbase\Wallet\Value\Money;
+use Coinbase\Wallet\Enum\Param;
+
 class CoinBaseWithdrawals
 {
+
+    //List withdrawals
+    public function getWithdrawals($client, $account)
+    {
+        $withdrawals = $client->getAccountWithdrawals($account);
+        return $withdrawals;
+    }
+
+
+    //Get withdrawal
+    public function getWithdrawalById($client, $account, $withdrawalId)
+    {
+        $withdrawal = $client->getAccountWithdrawal($account, $withdrawalId);
+        return $withdrawal;
+    }
+
+
+    //Withdraw funds
+    public function setWithdrawFounds($client, $account, $amount)
+    {
+        $withdrawal = new Withdrawal();
+        $withdrawal->setAmount($amount, CurrencyCode::BTC);
+        $client->createAccountWithdrawal($account, $withdrawal);
+
+        //Commit a withdrawal
+        //You only need to do this if you pass commit=true when you call the withdrawal method.
+        //$client->createAccountWithdrawal($account, $withdrawal, [Param::COMMIT => false]);
+        //$client->commitWithdrawal($withdrawal);
+    }
 
 }
