74dacc3a34af9a465fc19b519e86232f4d8f22ab
Germangalia <germangalia@iesebre.com>
2016-04-19 19:11:51 +0200

    Add market data class in CoinBase

---
+++ app/CoinBaseAPI/CoinBaseMarketData.php
@@ -0,0 +1,53 @@
+<?php
+
+
+class CoinBaseMarketData
+{
+
+    //List supported native currencies
+    public function getNativeCurrencies($client)
+    {
+        $currencies = $client->getCurrencies();
+        return $currencies;
+    }
+
+    //List exchange rates
+    public function getExchangeRates($client)
+    {
+        $rates = $client->getExchangeRates();
+        return $rates;
+    }
+
+    //Buy price
+    public function getBuyPrice($client)
+    {
+        $buyPrice = $client->getBuyPrice();
+        return $buyPrice;
+    }
+
+
+    //Sell price
+    public function getSellPrice($client)
+    {
+        $sellPrice = $client->getSellPrice();
+        return $sellPrice;
+    }
+
+
+    //Spot price
+    public function getSpotPrice($client)
+    {
+        $spotPrice = $client->getSpotPrice();
+        return $spotPrice;
+    }
+
+
+    //Current server time
+    public function getServerTime($client)
+    {
+        $time = $client->getTime();
+        return $time;
+    }
+
+
+}
