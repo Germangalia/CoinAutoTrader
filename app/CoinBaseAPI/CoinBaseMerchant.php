a88ad209a94c0069537cddc1407cb25b7b6b095f
Germangalia <germangalia@iesebre.com>
2016-04-19 21:07:48 +0200

    Add verifying merchant callback methods in CoinBaseMerchant class.

--- app/CoinBaseAPI/CoinBaseMerchant.php
+++ app/CoinBaseAPI/CoinBaseMerchant.php
@@ -11,4 +11,15 @@ class CoinBaseMerchant
         return $merchant;
     }
 
+
+    //Verifying merchant callbacks
+    //Note: Only production callbacks can be verified. Callbacks issued by the sandbox will always return false below.
+    public function verifyingMerchantCallbacks($client)
+    {
+        $raw_body = file_get_contents('php://input');
+        $signature = $_SERVER['HTTP_CB_SIGNATURE'];
+        $authenticity = $client->verifyCallback($raw_body, $signature); // boolean
+        return $authenticity;
+    }
+
 }
