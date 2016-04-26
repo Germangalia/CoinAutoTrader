dd6993b58232ca6a70728cfb0b08c6a3a8c9d689
Germangalia <germangalia@iesebre.com>
2016-04-26 17:45:22 +0200

    Make constants.php class

---
+++ config/constants.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    //Commission for trader transaction (1%)
+    'TRADER_COMMISSION' => 0.01,
+
+    //Iitial investment quantity (60%)
+    'INITIAL_CAPITAL_INVEST' => 0.6,
+
+    //CFAV percentage (10%)
+    'CFAV_PERCENT' => 0.1,
+];
